6479af61e2a27d687b419c194d3b99a3cabb7e66
Brian Moore <bmoore@book.com>
2015-12-05 14:23:47 -0500

    Front end build working

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,33 @@
 var gulp = require("gulp");
 var source = require("vinyl-source-stream");
 var browserify = require("browserify");
+var watchify = require("watchify");
 var babelify = require("babelify");
-var watch = require("gulp-watch");
 
-function bundle() {
-    return browserify("./src/js/main.js")
-            .transform(babelify, { presets: ["es2015", "react"] })
-            .bundle()
-            .on("error", function (e) {
-                console.log(e.message);
-            })
-            .pipe(source("bundle.js"))
-            .on("error", function (e) {
-                console.log("source error");
-            })
-            .pipe(gulp.dest("./public/js"));
+function onError(e) {
+    console.error("ERROR: " + e.message);
 }
 
-gulp.task("bundle-js", bundle);
+function bundleIt() {
+    function bundle() {
+        return b.bundle()
+            .on("error", onError)
+            .pipe(source("bundle.js"))
+            .pipe(gulp.dest("public/js"));
+    }
 
-gulp.task("default", function () {
-    bundle();
-    gulp.watch("./src/js/**/*.js", ["bundle-js"]);
-});
+    var b = browserify({
+            entries: ["src/js/main.js"],
+            cache: {},
+            packageCache: {},
+            plugin: [watchify]
+        })
+        .transform(babelify.configure({
+            presets: ["es2015", "react"]
+        }))
+        .on("update", bundle);
+
+    return bundle();
+}
+
+gulp.task("default", bundleIt);
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
-class Hello extends React.component {
+class Hello extends React.Component {
     render() {
-        return <h1>Hello</h1>
+        return <h1>Hello</h1>;
     }
 }
 
-React.render(<Hello />, document.querySelector(".main"));
+ReactDOM.render(<Hello />, document.querySelector(".main"));
